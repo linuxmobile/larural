8dc149504cbe1df7aaad88b18950edea80df64fc
linuxmobile <bdiez19@gmail.com>
2023-07-30 19:00:14 -0300

    Style: adding theme colors & more

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,16 @@ module.exports = {
       fontFamily: {
         'mona-sans': ['"Mona Sans"'],
       },
+      colors: {
+        'rural-black': '#070A09',
+        'rural-blue': '#1D3557',
+        'rural-green': '#386154',
+        'rural-green-hard': '#274038',
+        'rural-yellow': '#F2DB96',
+        'rural-red': '#B94A65',
+        'rural-white': '#ECFFEB',
+        'rural-sky': '#A8DADC'      
+      },
     },
 	},
 	plugins: [],
